650165aa0208dd8286aef713a5198569287ca893
albberrrt <albberrrt.yayay@gmail.com>
2024-01-22 19:31:27 -0300

    feat:added everything

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,77 @@
-import Image from "next/image";
+"use client"
 
+import { fiel, grecia, praia, theo } from "@/assets";
+import { Button } from "@/components/ui/button";
+import Image, { StaticImageData } from "next/image";
+import { useState } from "react";
+
+type productThumbType = "grecia" | "fiel" | "praia" | "theo"
 export default function Home() {
+  const [productThumb, setProductThumb] = useState<StaticImageData>(grecia)
+
+  const setProductPressed = (image: StaticImageData) => {
+    console.log(productThumb)
+
+    setProductThumb(image)
+
+    console.log(productThumb)
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    <>
+      <header className="flex min-w-full flex-row items-center justify-between px-12 py-6 border-b">
+        <h1 className="text-2xl font-bold text-gray-900">Vendas de Namoração Online Pro!</h1>
+      </header>
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <section className="flex gap-8 justify-center">
+          <div className="flex flex-row">
+            <div className="flex flex-col gap-1 pr-4">
+              <button onClick={() => setProductPressed(grecia)}>
+                <Image className="transition ease-in-out hover:opacity-50" alt="grecia" src={grecia} width={120} />
+              </button>
+              <button onClick={() => setProductPressed(fiel)}>
+                <Image className="transition ease-in-out hover:opacity-50" alt="fiel" src={fiel} width={120} />
+              </button>
+              <button onClick={() => setProductPressed(praia)}>
+                <Image className="transition ease-in-out hover:opacity-50" alt="praia" src={praia} width={120} />
+              </button>
+              <button onClick={() => setProductPressed(theo)}>
+                <Image className="transition ease-in-out hover:opacity-50" alt="theo" src={theo} width={120} />
+              </button>
+            </div>
+            <Image alt="Thumb" src={productThumb} width={480}></Image>
+          </div>
+          <div className="flex flex-col gap-4 w-2/5 text-gray-900">
+            <div className="flex flex-col gap-5">
+              <h3 className="font-semibold text-3xl">Relacionamento Vitalício Chiquita Bonita with Albert</h3>
+              <div className="flex flex-col gap-1">
+                <span className="text-xl font-semibold flex items-center">2.880.000 beijinhos<span className="text-sm pl-2 text-bold text-blue-900"> -50% OFF</span></span>
+                <span className="text-base text-gray-950 line-through font=semibold opacity-40">5.760.000 beijinhos</span>
+              </div>
+              <div>
+                <span className="text-sm text-bold text-blue-900">28.800 x de 100 beijinhos sem juros</span>
+              </div>
+              <Button className="bg-blue-900 text-white py-5">Comprar</Button>
+            </div>
+          </div>
+        </section>
+        <section className="text-gray-900 flex flex-col gap-3 w-3/6 mt-10">
+          <h5 className="text-blue-900 text-lg font-semibold">Descrição</h5>
+          <p>Imagine uma vida ao lado de alguém que te ama incondicionalmente. Alguém que sempre estará lá para você, nos bons e nos maus momentos. Alguém que te faz rir, que te faz chorar, que te faz sentir vivo.</p>
+          <p>Isso é o que você vai encontrar com o relacionamento vitalício comigo.</p>
+          <p>Eu te amo demais Amor.Quero passar o resto da minha vida com você, construindo uma família, viajando o mundo, e criando memórias que durarão para sempre.</p>
+          <p>Eu sei que você pode estar pensando: "2880000 beijos é muito caro." Mas não é. É um investimento no seu futuro. É um investimento em um amor que nunca vai acabar.</p>
+          <p>Com o relacionamento vitalício comigo, você terá:</p>
+          <ul className="list-disc ml-6">
+            <li>Um Amor incondicional</li>
+            <li>Um companheiro para a vida toda</li>
+            <li>Bolo de fubá vou aprender a fazer te amo</li>
+            <li>Muito carinho e amor e massagem</li>
+            <li>Uma vida de felicidade e alegria</li>
+          </ul>
+          <p className="font-bold mt-1">Então, o que você está esperando? Compre agora mesmo e comece a viver com o amor da sua vida.</p>
+        </section>
+      </main>
+    </>
   );
 }
---
+++ src/assets/index.ts
@@ -0,0 +1,6 @@
+import fiel from "./fiel.jpg"
+import theo from "./theo.jpg"
+import praia from "./praia.jpg"
+import grecia from "./grecia.jpg"
+
+export { fiel, theo, praia, grecia }
